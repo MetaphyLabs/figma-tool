a456115b2476187a0dc6c7735b67f702e452d4e4
og-rahul <rahulp@metaphy.world>
2024-08-14 20:51:33 +0530

    build: init shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
     <main>
       <section>
         <div>
           <p>Hello World</p>
+          <Link href='/app'>App</Link>
         </div>
       </section>
     </main>
