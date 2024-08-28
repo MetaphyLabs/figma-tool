8da452e4f5bd097e746f866691706a76fc9f7357
og-rahul <rahulp@metaphy.world>
2024-08-28 14:27:11 +0530

    docs: add .env.example file

--- src/components/pages/app/analyze.tsx
+++ src/components/pages/app/analyze.tsx
@@ -1,9 +1,10 @@
-"use client";
-import { useAppDesignUploadStore } from "@/client/client/app-design-upload.store";
-import { useFigmaDesignUploadStore } from "@/client/client/figma-design-upload.store";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import MarkdownIt from "markdown-it";
+'use client';
+
+import { useAppDesignUploadStore } from '@/client/client/app-design-upload.store';
+import { useFigmaDesignUploadStore } from '@/client/client/figma-design-upload.store';
+import axios from 'axios';
+import MarkdownIt from 'markdown-it';
+import { useEffect, useState } from 'react';
 
 const md = new MarkdownIt();
 
@@ -20,18 +21,18 @@ async function imageToBase64(url: string): Promise<string> {
 
 async function getAnalysis(figmaImageData: ImageData, appImageData: ImageData) {
   const figmaImage = await imageToBase64(figmaImageData.url);
-  console.log("Figma Image", figmaImage);
+  console.log('Figma Image', figmaImage);
   const appImage = await imageToBase64(appImageData.url);
-  console.log("App Image", appImage);
+  console.log('App Image', appImage);
   const headers = {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   };
   const payload = {
-    model: "gpt-4o-mini",
+    model: 'gpt-4o-mini',
     messages: [
       {
-        role: "user",
+        role: 'user',
         content: `I have a design image and the coded output image of a Figma design. Analyze if the coded output image is according to my design. Find the mistakes and issues and suggest corrections. Give only the reasoning. Give the whole answer in advanced markdown format, nothing else.\nFigma Image: ![Figma Image](data:image/jpeg;base64,${figmaImage})\nApp Image: ![App Image](data:image/jpeg;base64,${appImage})`,
       },
     ],
@@ -39,16 +40,12 @@ async function getAnalysis(figmaImageData: ImageData, appImageData: ImageData) {
   };
 
   try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
-      payload,
-      { headers }
-    );
-    console.log("Resp", response.data);
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', payload, { headers });
+    console.log('Resp', response.data);
     return response.data;
   } catch (error) {
-    console.error("Error:", error);
-    return "Error: Failed to analyze the images.";
+    console.error('Error:', error);
+    return 'Error: Failed to analyze the images.';
   }
 }
 
@@ -58,45 +55,40 @@ interface ImageData {
 }
 
 export default function Analyze() {
-  const [analysis, setAnalysis] = useState<string>("");
+  const [analysis, setAnalysis] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const figmaImageData = useFigmaDesignUploadStore(
-    (state) => state.blob
-  ) as ImageData;
-  const appImageData = useAppDesignUploadStore(
-    (state) => state.blob
-  ) as ImageData;
+  const figmaImageData = useFigmaDesignUploadStore((state) => state.blob) as ImageData;
+  const appImageData = useAppDesignUploadStore((state) => state.blob) as ImageData;
 
   useEffect(() => {
     if (figmaImageData && appImageData) {
-      setAnalysis("");
+      setAnalysis('');
       setIsLoading(true);
       getAnalysis(figmaImageData, appImageData)
         .then((response) => {
-          if (typeof response === "string") {
+          if (typeof response === 'string') {
             setAnalysis(response);
           } else {
-            setAnalysis(response?.choices[0]?.message?.content ?? "");
+            setAnalysis(response?.choices[0]?.message?.content ?? '');
           }
         })
-        .catch((error) => setAnalysis("Error: Failed to analyze the images."))
+        .catch((error) => setAnalysis('Error: Failed to analyze the images.'))
         .finally(() => setIsLoading(false));
     } else {
-      setAnalysis(
-        "Please upload both the Figma image and the app image for analysis."
-      );
+      setAnalysis('Please upload both the Figma image and the app image for analysis.');
     }
   }, [figmaImageData, appImageData]);
 
   return (
     <div>
-      <div className="bg-muted/50 rounded-md">
-        <div className="relative h-[60vh] p-2 overflow-y-auto">
+      <div className='bg-muted/50 rounded-md'>
+        <div className='relative h-[60vh] p-2 overflow-y-auto'>
           {isLoading ? (
             <p>Loading analysis...</p>
           ) : (
-            <div className=" ">
+            <div className=''>
               {/* <h2>Analysis Result</h2> */}
+              {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
               <div dangerouslySetInnerHTML={{ __html: md.render(analysis) }} />
             </div>
           )}
